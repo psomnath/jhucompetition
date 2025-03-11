d576c360dfd95ada93ae9a5c83c522ff5e706c2c
Somnath Purkayastha <psomnath@outlook.com>
2025-03-10 20:59:40 -0700

    Updates

--- hashit.py
+++ hashit.py
@@ -1,16 +1,19 @@
 import hashlib
 
-def hashit(input_list):
+def hashit(user_account):
 
-  return_list = []
-  for data in input_list:
-    hash_object = hashlib.sha256()
-    hash_object.update(data.encode())
-    return_list.append(hash_object.hexdigest())
+  [uid, password] = user_account
+  hash_object = hashlib.sha256()
+    
+  hash_object.update(uid.encode())
+  uid_encrypt = hash_object.hexdigest()
+    
+  hash_object.update(password.encode())
+  password_encrypt = hash_object.hexdigest()
 
-  return return_list
+  return [uid_encrypt,password_encrypt]
 
-input_list = ['psomnath@gmail.com','pass1234']
-return_list = hashit(input_list)
-for l in return_list:
-    print(l)
+user_accounts = [['psomnath@gmail.com','pass1234'],['spurkay1@jhu.com','pass1234'], ['ayasin1@jhu.edu','pass1234']]
+for user_account in user_accounts:
+    user_account_encrypt = hashit(user_account)
+    print(user_account_encrypt)
